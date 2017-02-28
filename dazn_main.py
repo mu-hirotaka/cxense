02f97a8474f8c1eec9fe1273db60a218d5d5b8fd
hirop4 <hirop4@gmail.com>
2017-02-28 10:16:55 +0900

    Modify parameter

--- dazn_main.py
+++ dazn_main.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     sys.exit(1)
   elif len(sys.argv) == 1:
     today = datetime.datetime.now().replace(hour=0,minute=0,second=0,microsecond=0)
-    seven_days_ago = today - datetime.timedelta(days = 7)
-    start_time = unixtime(seven_days_ago)
+    eight_days_ago = today - datetime.timedelta(days = 8)
+    start_time = unixtime(eight_days_ago)
     end_time = start_time + 60*60*24*7 - 1
     dazn_gss_manipulator.write_down_weekly_kpi(start_time, end_time)
   else:
     target_date = datetime.datetime.strptime(sys.argv[1], '%Y%m%d')
-    seven_days_ago = target_date - datetime.timedelta(days = 7)
-    start_time = unixtime(seven_days_ago)
+    eight_days_ago = target_date - datetime.timedelta(days = 8)
+    start_time = unixtime(eight_days_ago)
     end_time = start_time + 60*60*24*7 - 1
     dazn_gss_manipulator.write_down_weekly_kpi(start_time, end_time)
